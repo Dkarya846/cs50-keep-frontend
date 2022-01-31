0e00a04f3908a59740077252396208641592848f
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-31 23:15:26 +0530

    cors fix

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -2,28 +2,28 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, (error) => {
-  const expectedErrors =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status <= 500;
+   const expectedErrors =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status <= 500;
 
-  if (!expectedErrors) {
-    console.log("Logging the error", error);
-    toast.error("An unexpected error occured ");
-  }
-  return Promise.reject(error);
+   if (!expectedErrors) {
+      console.log("Logging the error", error);
+      toast.error("An unexpected error occured ");
+   }
+   return Promise.reject(error);
 });
 
 function setJWT(jwt) {
-  axios.defaults.headers.common["x-auth-token"] = jwt;
+   // axios.defaults.headers.common["x-auth-token"] = jwt;
 }
 
 const http = {
-  get: axios.get,
-  post: axios.post,
-  put: axios.put,
-  delete: axios.delete,
-  setJWT,
+   get: axios.get,
+   post: axios.post,
+   put: axios.put,
+   delete: axios.delete,
+   setJWT,
 };
 
 export default http;
